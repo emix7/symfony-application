5c18e2377e42670b5d6d39f61349f8dc516d8e76
endroid <info@endroid.nl>
2015-04-06 22:24:00 +0200

    Remove prod cache clear from reset script

--- reset.sh
+++ reset.sh
@@ -1,5 +1,4 @@
 php app/console cache:clear --env=dev
-php app/console cache:clear --env=prod
 php app/console doctrine:cache:clear-metadata
 php app/console doctrine:cache:clear-query
 php app/console doctrine:cache:clear-result
